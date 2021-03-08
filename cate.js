4e9d32863b79cc1582b8f3ce099dbf21a0f724f4
gathonicatherine <79199524+gathonicatherine@users.noreply.github.com>
2021-03-08 18:24:33 +0300

    Add files via upload

---
+++ cate.js
@@ -0,0 +1,10 @@
+function fruits () {
+    var fruits= ["pineapple", "apple", "oranges", "avocado", "kiwi"];
+    fruits.forEach ((fruits) => {
+        
+        console.log(fruits)
+    });
+    
+}
+
+fruits()
